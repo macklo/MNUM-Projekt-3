c5c69ccf9e9441aedf7a09a9b027052e30a19b73
Maciej Kłos <maciek2608@gmail.com>
2015-05-07 10:00:26 +0200

    Bisekcja i w sprawozdanie

--- bisection.m
+++ bisection.m
@@ -1,8 +1,24 @@
-function [ w ] = bisection(a,b,iter)
+function [ w ] = bisection(a,b,eps,imax)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-    while(
-
-
+    iter = 1;
+    if(f(a)*f(b)>0)
+        error('Brak obszaru izolacji');
+    end
+    c = (a+b)/2;
+    while(abs(f(c))>eps)
+        if(iter>=imax)
+            error('osiagnieto imax');
+        end
+        
+        if(f(c) * f(a)<0)
+            b = c;
+        else
+            a = c;
+        end
+        c = (a+b)/2;
+        iter = iter+1;
+     end
+    w = c;
 end
 
